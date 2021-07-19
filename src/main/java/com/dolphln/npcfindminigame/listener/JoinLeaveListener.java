31ce1433e8de0b78d6475d5d1a9e84eb95140655
Dolphln <dolphingamerytof@gmail.com>
2021-07-19 17:17:52 +0200

    Adding player to the database

--- src/main/java/com/dolphln/npcfindminigame/listener/JoinLeaveListener.java
+++ src/main/java/com/dolphln/npcfindminigame/listener/JoinLeaveListener.java
@@ -2,11 +2,13 @@ package com.dolphln.npcfindminigame.listener;
 
 import com.dolphln.npcfindminigame.NPCFindMinigame;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public record JoinLeaveListener(NPCFindMinigame plugin) implements Listener {
 
@@ -15,6 +17,16 @@ public record JoinLeaveListener(NPCFindMinigame plugin) implements Listener {
         if (plugin.getNpcManager().didItStart()) {
             e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999, plugin.getConfigFile().getConfig().getInt("speed_power"), true, false, true));
         }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerJoinAsync(PlayerJoinEvent e) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                plugin.getDatabase().addPlayer(e.getPlayer().getUniqueId(), e.getPlayer().getDisplayName());
+            }
+        }.runTaskAsynchronously(plugin);
     }
 
     @EventHandler
