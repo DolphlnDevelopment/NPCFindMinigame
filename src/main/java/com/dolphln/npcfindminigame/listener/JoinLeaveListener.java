f8e14a3b9d279e156979eec17f725f44dd419924
Dolphln <dolphingamerytof@gmail.com>
2021-07-19 12:36:28 +0200

    Removing speed if the game is running

--- src/main/java/com/dolphln/npcfindminigame/listener/JoinLeaveListener.java
+++ src/main/java/com/dolphln/npcfindminigame/listener/JoinLeaveListener.java
@@ -4,6 +4,7 @@ import com.dolphln.npcfindminigame.NPCFindMinigame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -19,6 +20,13 @@ public class JoinLeaveListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent e) {
         if (plugin.getNpcManager().didItStart()) {
             e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999, plugin.getConfigFile().getConfig().getInt("speed_power"), true, false, true));
+        }
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent e) {
+        if (plugin.getNpcManager().isRunning()) {
+            e.getPlayer().removePotionEffect(PotionEffectType.SPEED);
         }
     }
 }
