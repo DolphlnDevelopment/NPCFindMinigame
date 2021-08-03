d49d4d02fafa62fd24843001b502c72df6e461bf
Dolphln <dolphingamerytof@gmail.com>
2021-08-03 09:08:34 +0200

    Added a better check for blocks

--- src/main/java/com/dolphln/npcfindminigame/utils/BlockUtils.java
+++ src/main/java/com/dolphln/npcfindminigame/utils/BlockUtils.java
@@ -4,8 +4,18 @@ import org.bukkit.Material;
 
 public class BlockUtils {
 
-    public static boolean isBlockStatic(Material material) {
-        return !(material == Material.AIR || material == Material.WATER || material == Material.LAVA);
+    private static Material[] materials = new Material[]{Material.AIR, Material.WATER, Material.LAVA, Material.GRASS, Material.SEAGRASS, Material.TALL_GRASS, Material.TALL_SEAGRASS, Material.FLOWER_POT};
+
+    public static boolean isBlockStatic(Material mat) {
+        if (!mat.isBlock()) return false;
+
+        for (Material material : materials) {
+            if (mat.equals(material)) return false;
+        }
+
+        if (mat.toString().toLowerCase().contains("potted") || mat.toString().toLowerCase().contains("LEAVES")) return false;
+
+        return true;
     }
 
 }
